645cf7eeb6b0ef87e025606990f8a3bf1555f37b
Alex Hapgood <ach1@vt.edu>
2019-10-04 15:57:14 -0400

    WIP 4
    !!(over-printing bug)

--- index.js
+++ index.js
@@ -12,20 +12,22 @@ function handleStart () {
 
 function renderQuestion () {
     $('#quiz-body').append($(
-        `<p>guest@quizbox:~/ $ {? => q${STORE.currentQuestion + 1}} {% => ${(STORE.score / 5) * 100}%}</p>
-        <p id="question-1">guest@quizbox:~/ $ ${STORE.questions[STORE.currentQuestion].question}</p>
-        <p id="answer-a">>> a: ${STORE.questions[STORE.currentQuestion].options.a}</p>
-        <p id="answer-b">>> b: ${STORE.questions[STORE.currentQuestion].options.b}</p>
-        <p id="answer-c">>> c: ${STORE.questions[STORE.currentQuestion].options.c}</p>
-        <p id="answer-d">>> d: ${STORE.questions[STORE.currentQuestion].options.d}</p>
-        <p id="submission">guest@quizbox:~/ $ answer? (a/b/c/d): <input type="text" class="response"/> </p>`
+        `<div>
+            <p>guest@quizbox:~/ $ {? => q${STORE.currentQuestion + 1}} {% => ${(STORE.score / 5) * 100}%}</p>
+            <p id="question-1">guest@quizbox:~/ $ ${STORE.questions[STORE.currentQuestion].question}</p>
+            <p id="answer-a">>> a: ${STORE.questions[STORE.currentQuestion].options.a}</p>
+            <p id="answer-b">>> b: ${STORE.questions[STORE.currentQuestion].options.b}</p>
+            <p id="answer-c">>> c: ${STORE.questions[STORE.currentQuestion].options.c}</p>
+            <p id="answer-d">>> d: ${STORE.questions[STORE.currentQuestion].options.d}</p>
+            <p id="submission">guest@quizbox:~/ $ answer? (a/b/c/d): <input type="text" class="response"/> </p>
+        </div>`
     ));
     handleAnswerSubmit();
 };
 
 function handleAnswerSubmit () {
     console.log('handleAnswerSubmit ran');
-    $(".response").on('keydown', function response (e) {
+    $("#terminal-body").on('keydown', ".response", function response (e) {
         if (e.key === "Enter"){
             if($(".response").val() === "a") {
                 checkAnswer("a");
@@ -37,6 +39,7 @@ function handleAnswerSubmit () {
                 checkAnswer("d");
             }
         }
+        $(`#quiz-body div:nth-child(${STORE.currentQuestion})`).attr("disabled","disabled");
     })
 };
 
@@ -51,6 +54,7 @@ function checkAnswer (resp) {
         console.log(`wrong! correct answer was ${STORE.questions[STORE.currentQuestion].answer}`);
         console.log("current question", STORE.currentQuestion, " current score: ", STORE.score);
     }
+    renderQuestion();
 }
 
 function start () {
